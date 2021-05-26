9132aed35adb68ec584ee8dfe883416608b173ff
Oleksandr Vyshnivskyi <Oleksandr.Vishnivskiy@livenau.net>
2021-05-26 18:10:21 +0300

    fix(auth): fixed saving phoneNumber

--- Server/Core/Core.Business/Auth/Models/CreateUserModel.cs
+++ Server/Core/Core.Business/Auth/Models/CreateUserModel.cs
@@ -8,6 +8,8 @@ namespace Core.Business.Auth.Models
 
         public string Email { get; set; }
 
+        public string PhoneNumber { get; set; }
+
         public string Password { get; set; }
 
         public string ConfirmPassword { get; set; }
